0629500408dc28e3d77993aaaf8160771c9f33ec
wsdx233 <18082792459@163.com>
2026-05-21 00:06:52 +0800

    Use release FMOD plugin in debug APKs

--- tools/android/sync-runtime-from-references.sh
+++ tools/android/sync-runtime-from-references.sh
@@ -20,7 +20,9 @@ require_dir "$ANDROID_SRC/libs"
 require_dir "$ANDROID_SRC/assets/dotnet_bcl"
 require_file "$ANDROID_SRC/gradle/wrapper/gradle-wrapper.jar"
 CRYPTO_JAR="$LAUNCHER_REFERENCE_ROOT/vendor/godot/modules/mono/thirdparty/libSystem.Security.Cryptography.Native.Android.jar"
+FMOD_PLUGIN_AAR="$S2_REFERENCE_ROOT/addons/fmod/libs/android/fmod-release.aar"
 require_file "$CRYPTO_JAR"
+require_file "$FMOD_PLUGIN_AAR"
 
 mkdir -p "$ANDROID_DST/libs" "$ANDROID_DST/assets" "$ANDROID_DST/gradle/wrapper"
 rsync -a --delete "$ANDROID_SRC/libs/" "$ANDROID_DST/libs/"
@@ -28,5 +30,10 @@ rsync -a --delete "$ANDROID_SRC/assets/dotnet_bcl/" "$ANDROID_DST/assets/dotnet_
 cp -f "$ANDROID_SRC/gradle/wrapper/gradle-wrapper.jar" "$ANDROID_DST/gradle/wrapper/gradle-wrapper.jar"
 cp -f "$CRYPTO_JAR" "$ANDROID_DST/libs/debug/libSystem.Security.Cryptography.Native.Android.jar"
 cp -f "$CRYPTO_JAR" "$ANDROID_DST/libs/release/libSystem.Security.Cryptography.Native.Android.jar"
+# Use the release FMOD Android plugin even for monoDebug APKs. The plugin
+# BuildConfig.DEBUG branch tries to load libfmodL/libfmodstudioL debug
+# libraries, while the packaged Godot/FMOD runtime uses libfmod/libfmodstudio.
+cp -f "$FMOD_PLUGIN_AAR" "$ANDROID_DST/libs/debug/fmod-release.aar"
+rm -f "$ANDROID_DST/libs/debug/fmod-debug.aar"
 
 printf 'Synced runtime artifacts from %s to %s\n' "$ANDROID_SRC" "$ANDROID_DST"
